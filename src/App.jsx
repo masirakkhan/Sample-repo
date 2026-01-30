724fa5adc225f8962d7ac44767d7101490304033
masira khan <masirak866@gmail.com>
2026-01-30 13:46:24 +0530

    created new setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
 
-
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home'
-import Aboute from './Pages/Aboute'
-import Product from './Pages/Product'
-import Service from './Pages/Service'
-import Contact from './Pages/Contact'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-
-
-const App = () => {
-
-
-  return (
-   
-      <BrowserRouter>
-       <div className=' '>   
-      <Header/>
-      <Routes>
-        <Route path='/'element={<Home/>}/>
-        <Route path='/aboute'element={<Aboute/>}/>
-        <Route path='/product'element={<Product/>} />
-        <Route path='/service'element={<Service/>} />
-        <Route path='/contact'element={<Contact/>}/>
-      </Routes>
-      <Footer/>
-      </div>
-      </BrowserRouter>
+function App(){
+  return(
+    <div>App</div>
   )
 }
 
